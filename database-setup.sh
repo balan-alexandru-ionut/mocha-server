f73ec5c1806211c0b647c3d15cd5ce284619a080
balan-alexandru-ionut <160235161+balan-alexandru-ionut@users.noreply.github.com>
2024-04-06 18:47:32 +0300

    Initial setup (#1)
    * chore: add mongodb dependency
    
    * chore: add dependency for viper
    
    * chore: add config file with properties for connecting to mongo
    
    * chore: add docker compose file to setup database and readme
    
    * chore: add packages for logging and reading configuration

---
+++ database-setup.sh
@@ -0,0 +1,2 @@
+export MOCHA_DATABASE_USERNAME="mocha"
+export MOCHA_DATABASE_PASSWORD="mocha-password"
---
+++ util/configuration.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"mocha-server/util/log"
+
+	"github.com/spf13/viper"
+)
+
+type configuration struct {
+	Environment *viper.Viper
+	Yaml        *viper.Viper
+}
+
+var (
+	configurationInstance *configuration = nil
+	environmentViper                     = viper.New()
+	yamlViper                            = viper.New()
+	logger                               = log.New("configuration")
+)
+
+func Configuration() *configuration {
+	if configurationInstance != nil {
+		return configurationInstance
+	} else {
+		environmentViper.SetEnvPrefix("mocha")
+		environmentViper.AutomaticEnv()
+
+		yamlViper.SetConfigName("config")
+		yamlViper.SetConfigType("yaml")
+		yamlViper.AddConfigPath("$HOME/.config/mocha")
+		yamlViper.AddConfigPath(".")
+		err := yamlViper.ReadInConfig()
+		if err != nil {
+			logger.Panic(err)
+		}
+
+		configurationInstance = &configuration{
+			Environment: environmentViper,
+			Yaml:        yamlViper,
+		}
+
+		return configurationInstance
+	}
+}
